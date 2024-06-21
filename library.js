830f2a662d685343fe0600097f1471a7c09a057c
Simba Ndewere <ndeweresimba@gmail.com>
2024-06-21 19:10:49 +0100

    create functions for creating book, adding book to array and displaying book

--- library.js
+++ library.js
@@ -1,14 +1,17 @@
-function Book(title,author,totalPages,read){
+const myLibrary = [];
+
+function Book(title, author, pages){
     this.title = title;
     this.author = author;
-    this.totalPages = totalPages;
-    this.read = read;
-    this.info = function(){
-        return this.title + " by " + this.author + ", " + this.totalPages + " , "
-        + this.read;
-    }
+    this.pages = pages;
 }
 
-const book1 = new Book('Two Weeks in Novemeber', 'Douglas Rodgers', '241 pages', 'finished book');
+function addBookToLibrary(){
+    
+}
 
-console.log(book1.info());
+function displayBooks(){
+    for(let a = 0; a < myLibrary.length; a++){
+        //print books
+    }
+}
