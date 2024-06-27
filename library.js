7d384935cad3ce6a1d0b899eab1fa90de4ea3365
Simba Ndewere <ndeweresimba@gmail.com>
2024-06-27 15:43:37 +0100

    add cards to library after submitting form

--- library.js
+++ library.js
@@ -7,6 +7,9 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 310, true);
+const book2 = new Book("Two Weeks In November", "Douglas Rogers", 241, false);
+
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
@@ -17,9 +20,68 @@ function createBook(title, author, pages, read) {
 }
 
 function displayBooks() {
-    for (let a = 0; a < myLibrary.length; a++) {
-        console.log(myLibrary[a]);
-    }
+    createCard(myLibrary[myLibrary.length-1]);
+}
+
+window.onload = function () {
+    createCard(book1);
+    createCard(book2);
+};
+
+function createCard(book) {
+
+    const right = document.querySelector(".right");
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.setAttribute("id", "1");
+    const cardTitle = document.createElement("div");
+    cardTitle.textContent = book.title;
+    const cardAuthor = document.createElement("div");
+    cardAuthor.textContent = book.author;
+    const cardPages = document.createElement("div");
+    cardPages.textContent = book.pages + "pages";
+
+    const checkBoxRead = document.createElement("div");
+    checkBoxRead.classList.add("checkBox-Read");
+
+    const read = document.createElement("div");
+    read.textContent = "Read";
+    read.classList.add("readLabel");
+    const switchLabel = document.createElement("label");
+    switchLabel.classList.add("switch");
+    const switchInput = document.createElement("input");
+    switchInput.setAttribute("type", "checkbox");
+    switchInput.checked = book.read;
+    const span = document.createElement("span");
+    span.classList.add("slider");
+
+    switchLabel.appendChild(switchInput);
+    switchLabel.appendChild(span);
+
+    checkBoxRead.appendChild(read);
+    checkBoxRead.appendChild(switchLabel);
+
+    const buttonCard = document.createElement("div");
+    buttonCard.classList.add("buttonCard");
+    const buttonInput = document.createElement("input");
+    buttonInput.setAttribute("class", "btn");
+    buttonInput.setAttribute("type", "submit");
+    buttonInput.setAttribute("value", "Delete");
+    buttonCard.appendChild(buttonInput);
+
+    card.appendChild(cardTitle);
+    card.appendChild(cardAuthor);
+    card.appendChild(cardPages);
+    card.appendChild(checkBoxRead);
+    card.appendChild(buttonCard);
+
+    right.appendChild(card);
+
+    buttonInput.addEventListener("click", function(event) {
+        event.preventDefault();
+        right.removeChild(card);
+    });
 }
 
 function addBookButton() {
@@ -27,7 +89,6 @@ function addBookButton() {
     const buttonConst = document.querySelector(".button");
     const left = document.querySelector(".left");
     buttonConst.style.display = "none";
-    console.log("button removed");
 
     const modal = document.createElement("div");
     modal.classList.add("modal");
@@ -36,22 +97,9 @@ function addBookButton() {
     form.setAttribute("action", "");
     form.setAttribute("id", "custom-form");
     const h1 = document.createElement("h1");
-    h1.textContent = "book details"
+    h1.textContent = "book details";
     form.classList.add("form");
     h1.classList.add("h1");
-
-    const formGroup = document.createElement("div");
-    const authorLabel = document.createElement("label")
-    authorLabel.textContent = "author";
-    authorLabel.setAttribute("for", "");
-    formGroup.appendChild(authorLabel);
-
-    const authorInput = document.createElement("input");
-    authorInput.setAttribute("type", "text");
-    authorInput.setAttribute("required", "");
-    authorInput.setAttribute("name", "author");
-    authorInput.classList.add("form-control");
-    formGroup.appendChild(authorInput);
 
     const formGroup2 = document.createElement("div");
     const titleLabel = document.createElement("label")
@@ -63,8 +111,23 @@ function addBookButton() {
     titleInput.setAttribute("type", "text");
     titleInput.setAttribute("required", "");
     titleInput.setAttribute("name", "title");
+    titleInput.setAttribute("maxLength", "40");
     titleInput.classList.add("form-control");
     formGroup2.appendChild(titleInput);
+
+    const formGroup = document.createElement("div");
+    const authorLabel = document.createElement("label")
+    authorLabel.textContent = "author";
+    authorLabel.setAttribute("for", "");
+    formGroup.appendChild(authorLabel);
+
+    const authorInput = document.createElement("input");
+    authorInput.setAttribute("type", "text");
+    authorInput.setAttribute("required", "");
+    authorInput.setAttribute("name", "author");
+    authorInput.setAttribute("maxLength", "40");
+    authorInput.classList.add("form-control");
+    formGroup.appendChild(authorInput);
 
     const formGroup3 = document.createElement("div");
     const pagesLabel = document.createElement("label")
@@ -86,7 +149,6 @@ function addBookButton() {
     readLabel.setAttribute("for", "");
     formGroup4.appendChild(readLabel);
 
-
     const readInput = document.createElement("input");
     readInput.setAttribute("type", "checkbox");
     readInput.classList.add("form-control");
@@ -100,8 +162,8 @@ function addBookButton() {
     enterButton.classList.add("btn");
 
     form.appendChild(h1);
-    form.appendChild(formGroup);
     form.appendChild(formGroup2);
+    form.appendChild(formGroup);
     form.appendChild(formGroup3);
     form.appendChild(formGroup4);
     form.appendChild(enterButton);
@@ -112,7 +174,7 @@ function addBookButton() {
 
     form.addEventListener("submit", (e) => {
         e.preventDefault();
-        
+
         let data = new FormData(e.target);
         const author = data.get("author");
         const title = data.get("title");
@@ -121,5 +183,10 @@ function addBookButton() {
 
         createBook(title, author, pages, read);
         displayBooks();
+        const modal = document.querySelector(".modal");
+        const left = document.querySelector(".left");
+        left.removeChild(modal);
+        const buttonConst = document.querySelector(".button");
+        buttonConst.style.display = "block";
     });
 }
